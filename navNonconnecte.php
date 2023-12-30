16f59c625a4232e9cd1ba6c7ed73fdc8c5e2ccff
Chelabi Maroua <125988911+Mar27oua@users.noreply.github.com>
2023-12-30 14:10:31 +0100

    Update navNonconnecte.php

--- navNonconnecte.php
+++ navNonconnecte.php
@@ -3,7 +3,7 @@
 <nav id="navbar" class="navbar">
         <ul>
           <li><a class="active " href="index.php">Home</a></li>
-          <li><a href="connexion.php">Sign in/Sign up</a></li>
+          <li><a href="ConnexionPage.php">Sign in/Sign up</a></li>
           <li><a href="compagnie.php">Compagnie</a></li>
         </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
