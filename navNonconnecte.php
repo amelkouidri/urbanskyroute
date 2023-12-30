2ab1bac21b16141c73551ed9a519f33974443151
Chelabi Maroua <125988911+Mar27oua@users.noreply.github.com>
2023-12-30 14:24:04 +0100

    Update navNonconnecte.php

--- navNonconnecte.php
+++ navNonconnecte.php
@@ -4,7 +4,6 @@
         <ul>
           <li><a class="active " href="index.php">Home</a></li>
           <li><a href="ConnexionPage.php">Sign in/Sign up</a></li>
-          <li><a href="compagnie.php">Compagnie</a></li>
         </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
       </nav><!-- .navbar -->
