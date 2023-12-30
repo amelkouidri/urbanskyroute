8730ae83bb39db2436063a65ffc0e461be6fa97f
Chelabi Maroua <125988911+Mar27oua@users.noreply.github.com>
2023-12-30 13:44:20 +0100

    Update navConnecte.php
    Jai ajoute  "etes vous sur de vouloir vous deconnecter "

--- navConnecte.php
+++ navConnecte.php
@@ -6,7 +6,16 @@
         <li><a href="compagnie.php">Compagnie</a></li>
             <li><a href="profil.php">profil</a></li>
             <li><a href="rechercheTrajet.php">Recherche Trajets</a></li>
-            <li><a href="index.php">Déconnexion</a></li>
+            <li><a href="#"  onclick="confirmLogout()">Déconnexion</a></li>
+
+                    <script>
+                    function confirmLogout() {
+                    var confirmation = confirm("Êtes-vous sûr de vouloir vous déconnecter ?");
+                    if (confirmation) {
+                    window.location.href = "index.php"; 
+                    }
+                    }
+                    </script>
         </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
       </nav><!-- .navbar -->
