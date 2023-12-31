f716d3d826bd692dbab8142a4ce1c5cb32e1f6f0
Chelabi Maroua <125988911+Mar27oua@users.noreply.github.com>
2023-12-31 21:08:32 +0100

    Update Connexion.php

--- Connexion.php
+++ Connexion.php
@@ -1,10 +1,9 @@
-
 <?php
-//******************************************************************************************** */
 $serveur = "localhost:3307";
 $utilisateur = "root";
 $motDePasse = "";
 $baseDeDonnees = "urbanskyroute";
+
 $connexion = new mysqli($serveur, $utilisateur, $motDePasse, $baseDeDonnees);
 
 if ($connexion->connect_error) {
@@ -12,7 +11,7 @@ if ($connexion->connect_error) {
 }
 
 if (!isset($_POST["user"]) || !isset($_POST["pass"])) {
-    echo "Nom d'utilisateur et mot de passe non spécifiés.";
+    echo "Veuillez fournir un nom d'utilisateur et un mot de passe.";
     exit();
 }
 
@@ -20,15 +19,16 @@ $sql = "SELECT * FROM user WHERE username = ? AND motdepasse = ?";
 $requete = $connexion->prepare($sql);
 
 $requete->bind_param("ss", $_POST["user"], $_POST["pass"]);
-
 $requete->execute();
 
 $resultat = $requete->get_result();
 
 if ($resultat->num_rows > 0) {
+    $row = $resultat->fetch_assoc();
     echo "Connexion réussie";
 } else {
     echo "Nom d'utilisateur ou mot de passe incorrect.";
 }
+
 $connexion->close();
 ?>
